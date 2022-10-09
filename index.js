5dc8f934ab10b6a84470ab669f07adca7d4b25c2
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-10-09 11:04:27 -0500

    added prompt function

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
+const md = require('./utils/generateMarkdown');
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const markDownObj = [];
+
+/*
+ [
+    title: {
+        header: # <name-of-project>
+    },
+    description: {
+        header: ## Description,
+        info: ["This is the des"]
+    },
+ ] 
+ */
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "checkbox",
+        name: "sections",
+        message: "Which sections should be included?",
+        choices: [
+            "description",
+            "installation",
+            "usage",
+            "license",
+            "contributing",
+            "tests",
+            "questions"]
+    },
+];
+
+// Prompt user for sections
+const getSections = async () => {
+    const sections = await inquirer.prompt(questions);
+    // console.log(sections);
+    return sections;
+}
+
+// Prompt user for section info
+const getInfo = async (section) => {
+    const infoInput = await inquirer.prompt([
+        {
+            type: "input",
+            name: section,
+            message: `Enter info for ${section}`,
+        }
+    ]
+    );
+    return infoInput;
+}
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const sections = await getSections();
+
+    for (const section of sections.sections) {
+        let info = await getInfo(section);
+        markDownObj.push(info)
+    }
+
+    console.log(markDownObj)
+}
 
 // Function call to initialize app
 init();
